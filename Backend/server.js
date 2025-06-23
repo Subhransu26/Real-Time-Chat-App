3a9146a56ec2b3f2624333d776f4950afc3d6837
Subhransu26 <subhransusahu95@gmail.com>
2025-06-23 13:55:10 +0530

    Initial commit: Full-stack real-time chat app

--- Backend/server.js
+++ Backend/server.js
@@ -13,12 +13,29 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(express.json());
 
 // Routes
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ------------------ Deployment ----------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../Frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "../Frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// ------------------ Deployment ----------------------------
 
 // Error Handling Middlewares
 app.use(notFound);
